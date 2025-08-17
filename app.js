1e95ccebb42395464f7bb4fbac91aaedeb84cc34
Maxine Athos <heyymax@icloud.com>
2025-08-16 22:42:49 -0300

    add app.js | update html and css

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const URL =  "https://api.github.com/users/maxineathos"
-fetch(URL)
-.then(res=>res.json())
-.then(data=>{
-    console.log(data)
-})
+const searchGithub = async (event) => {
+    if (event) event.preventDefault(); // previne reload do form
+    const username = document.getElementById("searchInput").value;
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const detailsContainer = document.querySelector(".details");
+    const data = await response.json();
+
+    if (response.ok) {
+        detailsContainer.style.display = "flex";
+        document.getElementById("result").innerHTML = `
+            <div class="profile">
+                <div class="profile-image">
+                    <img src="${data.avatar_url}" />
+                    <h2 class="name">${data.name || data.login}</h2>
+                    <p class="username">@${data.login}</p>
+                    <p class="bio">${data.bio || "This account doesn't have a bio." }</p>
+                </div>
+            </div>`;
+    } else {
+        alert(data.message);
+    }
+}
